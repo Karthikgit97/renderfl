345d7dc1e2cdf9a829fa073de602e98f68bad26a
Karthikgit97 <101712141+Karthikgit97@users.noreply.github.com>
2023-01-20 15:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
+import datetime
 import sqlite3
-import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+db = 'selva.db'
 
-@app.route('/create1')
+@app.route('/')
+def home():
+    return redirect("https://deciduo.vercel.app", code=302)
+
+@app.route('/create_db')
 def create():
-    conn = sqlite3.connect('test.db')
+    conn = sqlite3.connect(db)
     print ("Opened database successfully")
 
-    conn.execute('''CREATE TABLE COMPANY
-            (ID            INT     NOT NULL,
-            NAME           TEXT    NOT NULL,
-            AGE            INT     NOT NULL,
-            ADDRESS        CHAR(50),
-            SALARY         REAL);''')
-    print ("Table created successfully")
-
+    conn.execute('''CREATE TABLE data
+            (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            TIMESTAMP      TEXT    NOT NULL,
+            VALUE_1        INT     NOT NULL,
+            VALUE_2        INT     NOT NULL,
+            VALUE_3        INT     NOT NULL,
+            VALUE_4        INT     NOT NULL,
+            VALUE_5        INT     NOT NULL,
+            VALUE_6        INT     NOT NULL);''')
     conn.close()
 
+    print ("Table created successfully")
     return 'table created successfully'
 
-@app.route('/insert')
+@app.route('/insert', methods=['GET'])
 def insert():
-    conn = sqlite3.connect('test.db')
+    value1 = request.args.get('value1')
+    value2 = request.args.get('value2')
+    value3 = request.args.get('value3')
+    value4 = request.args.get('value4')
+    value5 = request.args.get('value5')
+    value6 = request.args.get('value6')
+    print(value1, value2, value3, value4, value6)
+    print(str(datetime.datetime.now()))
+
+    conn = sqlite3.connect(db)
     print ("Opened database successfully")
 
     # conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
     #     VALUES (1, 'Paul', 32, 'California', 20000.00 )")
 
-    conn.execute("INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(?, ?, ?, ?, ?)", 
-                    (str(random.seed(100)), 'Paul', 32, 'California', 20000.00))
+    conn.execute("INSERT INTO data (TIMESTAMP, VALUE_1, VALUE_2, VALUE_3, VALUE_4, VALUE_5, VALUE_6) VALUES(?, ?, ?, ?, ?, ?, ?)", 
+                    (str(datetime.datetime.now()), value1, value2, value3, value4, value5, value6))
 
     conn.commit()
     print ("Records created successfully")
@@ -42,24 +55,20 @@ def insert():
 
     return 'record inserted successfully'
 
-@app.route('/select')
+@app.route('/view')
 def select():
-    name = ""
-    conn = sqlite3.connect('test.db')
+    name = "12"
+    conn = sqlite3.connect(db)
     print ("Opened database successfully")
 
-    cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
-    for row in cursor:
-        print("ID = ", row[0])
-        print("NAME = ", row[1])
-        print("ADDRESS = ", row[2])
-        print("SALARY = ", row[3])
-        name = row[1]
+    cursor = conn.execute("SELECT * from data")
+    results = cursor.fetchall()
+    conn.close()
 
     print ("Operation done successfully")
     conn.close()
 
-    return name
+    return render_template("view.html", data=results)
 
 if __name__ == '__main__':
     app.run()
